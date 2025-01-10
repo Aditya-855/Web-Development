28091d41396e3e5fc6e6a0d63a336579e19f0d01
Aditya-855 <139979027+Aditya-855@users.noreply.github.com>
2025-01-10 22:12:51 +0530

    JavaScript files for learning variables

---
+++ JavaScript/Learning Variables/index.js
@@ -0,0 +1,14 @@
+// var age = 25;
+
+// if(true){
+//     //var age = 30;
+//     console.log(age);
+// }
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve(); // 25
+var x=10;
+var x=20;
